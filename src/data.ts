154702bd84089afffbfde19fcc1100b43635c0eb
Tanush Maurya <tanushmaurya018@gmail.com>
2024-01-29 20:07:46 +0530

    made map function for menu and its type and replaced it with hardcoded text

--- src/data.ts
+++ src/data.ts
@@ -63,3 +63,41 @@ export const FeaturedProduct: Products = [
   },
   
 ];
+export type Menu = {
+  id?: number;
+  name: string;
+  desc?: string;
+  img: string;
+  theme?:{
+    textcolor?:string;
+    buttoncolor?:string;
+    bgcolor?:string;
+  };
+};
+type MenuArray=Menu[]
+export const Menus:MenuArray = [
+  {
+    id: 1,
+    name: "Cheezy Pizzas",
+    desc: "jbfdkjfbdkjbd",
+    img: "/temporary/m3.png",
+  },
+  {
+    id: 2,
+    name: "Italian Pasta",
+    desc: "jbfdkjfbdkjbd",
+    img: "/temporary/m1.png",
+  },
+  {
+    id: 3,
+    name: "Juicy Burgers",
+    desc: "jbfdkjfbdkjbd",
+    img: "/temporary/m2.png",
+    theme:{
+      textcolor:"text-black",
+      buttoncolor:"bg-black",
+      bgcolor:"bg-white",
+    },
+  },
+
+];
